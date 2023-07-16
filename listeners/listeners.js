51031f11e73b98cc2522b167b32246c4dfe5499b
HASHI99M <zmalek2018@gmail.com>
2023-07-16 18:15:19 +0300

    update

--- listeners/listeners.js
+++ listeners/listeners.js
@@ -22,51 +22,24 @@ module.exports.registerListeners = async (socket, io, ctx) => {
     })
     socket.on("messages", async (data) => {
 
-        let conversation = await ctx.conversations.findOne({
-            where: {
-                [Op.or]: [
-                    {
-                        sender_id: data.from_id,
-                        receiver_id: data.to_id
-                    },
-                    {
-                        sender_id: data.to_id,
-                        receiver_id: data.from_id
-                    }
-                ]
-            }
-        })
-        if (!conversation) {
-
-            conversation = await ctx.conversations.create({
-                sender_id: data.from_id,
-                receiver_id: data.to_id,
-
-            })
-        }
-        let ret = await ctx.messages.create({
-            from_id: data.from_id,
-            conversation_id: conversation.id,
-            to_id: data.to_id,
-            text: data.text,
-            media: data.media,
-            seen: 0,
-        })
-        let media = null
-        if(data.media){
-            media = ctx.globalconfig['site_url']+'/'+data.media;
-        }
-        await io.to(data.to_id).emit("messages", {
-            id: ret.id,
-            from_id: data.from_id,
-            conversation_id: conversation.id,
-            to_id: data.to_id,
-            media: media,
-            text: data.text,
-            updated_at: ret.updated_at,
-            created_at: ret.created_at,
-        });
+        await io.to(data.to_id).emit("messages", data);
 
     })
+
+    socket.on('disconnect', async (reason) => {
+        console.log('a user disconnected ' + socket.id + " " + reason)
+        let hash = ctx.socketIdUserHash[socket.id]
+        let user_id = ctx.userHashUserId[hash]
+        ctx.userIdCount[user_id] > 0 ? ctx.userIdCount[user_id] = ctx.userIdCount[user_id] - 1 : delete ctx.userIdCount[user_id]
+        if (ctx.userIdCount[user_id] === 0) {
+            delete ctx.userIdCount[user_id]
+            delete ctx.userHashUserId[hash]
+        }
+        if (ctx.userIdSocket[user_id]) {
+            ctx.userIdSocket[user_id] = ctx.userIdSocket[user_id].filter(d => d.id != socket.id)
+        }
+        ctx.userIdExtra = {}
+        delete ctx.socketIdUserHash[socket.id]
+    });
 
 }  
